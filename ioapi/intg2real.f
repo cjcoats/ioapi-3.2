f4aa257a095e296febc3182fa15adba6c5b3ed16
cjcoats <cjcoats@email.unc.edu>
2017-01-30 13:54:35 -0500

    FLOAT() ~~> REAL for "g95" -- CJC

--- ioapi/intg2real.f
+++ ioapi/intg2real.f
@@ -2,15 +2,15 @@
         SUBROUTINE INTG2REAL( SIZE, INTG, GRID )
 
 C***********************************************************************
-C Version "$Id: intg2real.f 219 2015-08-17 18:05:54Z coats $"
+C Version "$Id: intg2real.f 448 2017-01-30 18:53:50Z coats $"
 C EDSS/Models-3 I/O API.
 C Copyright (C) 1992-2002 MCNC and Carlie J. Coats, Jr.,
 C (C) 2003-2010 by Baron Advanced Meteorological Systems.
 C Distributed under the GNU LESSER GENERAL PUBLIC LICENSE version 2.1
 C See file "LGPL.txt" for conditions of use.
 C.........................................................................
-C  subroutine INTG2REAL body starts at line  44
-C  subroutine INT82REAL body starts at line  44
+C  subroutine INTG2REAL body starts at line  46
+C  subroutine INT82REAL body starts at line  75
 C
 C  FUNCTION:
 C       convert INTEGER input array INTG( SIZE ) to REAL
@@ -24,6 +24,7 @@ C  REVISION  HISTORY:
 C       prototype 6/95 by CJC
 C       Modified 03/2010 by CJC: F9x changes for I/O API v3.1
 C       Modified 02/2015 by CJC for I/O API 3.2: support for M3INT8 variables
+C       Modified 01/2017 by CJC: M3INT8 case, FLOAT() ~~> REAL() for g95
 C***********************************************************************
 
         IMPLICIT NONE
@@ -72,7 +73,7 @@ C***********************************************************************
 C   begin body of subroutine  INT2REAL
 
         DO  I = 1, SIZE
-            GRID( I ) = FLOAT( INT8( I ) )
+            GRID( I ) = REAL( INT8( I ) )
         END DO
 
         RETURN
